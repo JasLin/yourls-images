2691bba46465f51fc8fcc272186be26999c0d52f
jas <jaslin00@gmail.com>
2020-10-16 17:06:35 +0800

    增加307的支持

--- Always-307/plugin.php
+++ Always-307/plugin.php
@@ -1,18 +1,18 @@
 <?php
 /*
 Plugin Name: Always-307 Plugin
-Plugin URI: https://github.com/tinjaw/Always-307
+Plugin URI: https://github.com/jaslin/Always-307
 Description: Send a 307 (temporary) redirect instead of 301 (permanent) for sites where shortlinks may change.
 Version: 1.0
-Author: Tinjaw
-Author URI: https://github.com/tinjaw
+Author: jaslin
+Author URI: https://github.com/jaslin
 */
 
 // No direct call
 if( !defined( 'YOURLS_ABSPATH' ) ) die();
 
-yourls_add_filter( 'redirect_code', 'tinjaw_307_redirection' );
+yourls_add_filter( 'redirect_code', 'jaslin_307_redirection' );
 
-function tinjaw_307_redirection( $code, $location ) {
+function jaslin_307_redirection( $code, $location ) {
     return 307;
 }
--- Dockerfile
+++ Dockerfile
@@ -1,2 +1,3 @@
 From yourls:latest
 COPY ./Always-302 /var/www/html/user/plugins/Always-302
+# COPY ./Always-307 /var/www/html/user/plugins/Always-307
